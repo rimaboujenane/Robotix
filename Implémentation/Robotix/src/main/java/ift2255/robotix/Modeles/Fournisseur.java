32032ec38c4904e1ef9ef6c555d067126bb5b2d9
Snigstew <david.shorten@umontreal.ca>
2024-08-05 15:47:31 -0400

    Resolve GestionFournisseurs.java

--- Implémentation/Robotix/src/main/java/ift2255/robotix/Modeles/Fournisseur.java
+++ Implémentation/Robotix/src/main/java/ift2255/robotix/Modeles/Fournisseur.java
@@ -5,6 +5,7 @@ package ift2255.robotix.Modeles;
  * Hérite des attributs et des méthodes de la classe User.
  */
 public class Fournisseur extends User {
+    private String compagnie;
 
     /**
      * Constructeur pour créer un nouveau fournisseur avec les informations spécifiées.
@@ -14,8 +15,14 @@ public class Fournisseur extends User {
      * @param password Le mot de passe du fournisseur
      * @param email L'email du fournisseur
      * @param telephone Le numéro de téléphone du fournisseur
+     * @param compagnie le nom de la compagnie
      */
-    public Fournisseur(String nom, String prenom, String password, String email, String telephone) {
+    public Fournisseur(String nom, String prenom, String password, String email, String telephone, String compagnie) {
         super(nom, prenom, password, email, telephone); // Appel du constructeur de la classe User
+        this.compagnie = compagnie;
+    }
+
+    public String getCompagnie() {
+        return compagnie;
     }
 }
