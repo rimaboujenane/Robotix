290334db4ae55b3565be5651a86b82ccf4d6b9db
cedricguevremont <cedric.guevremont@gmail.com>
2024-08-06 22:59:41 -0400

    testing test

--- Implémentation/Robotix/src/main/java/ift2255/robotix/Modeles/RegisterUser.java
+++
@@ -1,8 +0,0 @@
-package ift2255.robotix.Modeles;
-
-/**
- * Classe abstraite servant de base pour l'enregistrement des utilisateurs.
- * Cette classe est destinée à être étendue pour créer des implémentations spécifiques.
- */
-public abstract class RegisterUser {
-}
